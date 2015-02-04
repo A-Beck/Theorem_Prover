7cf313e38ebb0552c36b3008337a95b473d785f7
Andrew Becker <adb2jb@virginia.edu>
2015-02-04 00:34:05 +0000

    Added basic variable class

---
+++ datastructures.py
@@ -0,0 +1,14 @@
+# A file that defines the custom data structs used in our assignment
+
+class variable(object):
+    
+    """ Class that represents the basic state of a variable """
+    
+    def __init__(self, str_val="", bool_val=False):
+        """ object state includes string value and truth value """
+        self.string_value = str_val
+        self.truth_value = bool_val
+        
+    def __str__(self):
+        """ so str() operator can be used """
+        return self.string_value
