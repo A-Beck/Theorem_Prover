26eb2d115dc9b3013988d167ce1301bc244a9a16
Andrew Becker <adb2jb@virginia.edu>
2015-02-17 04:36:36 +0000

    Adding more tests and reformatting

--- src/test.py
+++ src/test.py
@@ -15,6 +15,7 @@ class Test(unittest.TestCase):
         var5 = Variable('C', 'Sam is skinny')
         var6 = Variable('D', 'Sam lives')
         var7 = Variable('E', 'Sam is vegan')
+        var8 = Variable('F', 'Sam has a sweet tooth')
 
         variables.extend([
             var1,
@@ -23,7 +24,8 @@ class Test(unittest.TestCase):
             var4,
             var5,
             var6,
-            var7
+            var7,
+            var8
         ])
 
         var1.truth_value = True
@@ -40,44 +42,48 @@ class Test(unittest.TestCase):
         self.exp1 = Expression('S&V')  # True 
         self.exp2 = Expression('!(B)')  # True
         self.exp3 = Expression('!(!(S|V)&(!B))')  # not (not (True and True) and ( not False)) --> True
-	self.exp4 = Expression('B|!A')  # (False or  not True) --> False
+        self.exp4 = Expression('B|!A')  # (False or  not True) --> False
        	self.exp5 = Expression('! (S&A) | (!A|B)')  # not (true and true) or (not true or false)) --> false 
 
-	 # set up rules
+	     # set up rules
 
         rule1 = Rule(Expression('S'), var2)
         rules.append(rule1)
 
         rule2 = Rule(self.exp2, var6)
         rules.append(rule2)
+        
+        rule3 = Rule(Expression('S'), var8)
+        rules.append(rule3)
 
     def test_query(self):
         self.assertTrue(query(self.exp1))
         self.assertTrue(query(self.exp2))
         self.assertTrue(query(self.exp3))
-	self.assertFalse(query(self.exp4))
-	self.assertFalse(query(self.exp5))	
+        self.assertFalse(query(self.exp4))
+        self.assertFalse(query(self.exp5))	
 
     def test_forward_chain(self):
-        pass
+        forward_chain(rules, facts_raw)
+        self.assertTrue(find_var(variables, 'F')in facts_raw)
 
     def test_why(self):
         """Print line testing why()"""
         why(Expression('A'))   # simplest case (true)
-	print '\n'
-	why(Expression('!A'))  # A simple not case (false)
-	print '\n'
-	why(self.exp1)  # simple (true) 'and' expression
-	print '\n'
-	why(Expression('!S&V'))   # simple (false) 'and' and not expression
-	print '\n'
-	why(Expression('B|A'))   # simple (true) 'or' expression
-	print '\n'
-	why(self.exp4)   # simple (false) 'or' and 'not' expression
-	print '\n'
-	why(self.exp3)  # complex (true) expression with 'ands' and 'nots' and 'ors'
-	print '\n'
-	why(self.exp5)  # complex (false) expression with all operators		
+        print '\n'
+        why(Expression('!A'))  # A simple not case (false)
+        print '\n'
+        why(self.exp1)  # simple (true) 'and' expression
+        print '\n'
+        why(Expression('!S&V'))   # simple (false) 'and' and not expression
+        print '\n'
+        why(Expression('B|A'))   # simple (true) 'or' expression
+        print '\n'
+        why(self.exp4)   # simple (false) 'or' and 'not' expression
+        print '\n'
+        why(self.exp3)  # complex (true) expression with 'ands' and 'nots' and 'ors'
+        print '\n'
+        why(self.exp5)  # complex (false) expression with all operators		
 
 
 if __name__ == '__main__':
